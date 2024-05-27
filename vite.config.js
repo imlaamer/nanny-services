de19df2d46c08db0afd5466e4fb3f0a2777a1d7b
Lolita Yenik <lolitayenik@ukr.net>
2024-05-27 03:17:49 +0300

    added versel json and updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-      plugins: [react(), svgr()],
-      resolve: {
-        alias: {
-          src: '/src',
-          components: '/src/components',
-          pages: '/src/pages',
-          assets: '/src/assets'
-        },
-      },
-      base: '/nanny-services/',
-    });
+  plugins: [react(), svgr()],
+  build: { sourcemap: true },
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      pages: '/src/pages',
+      assets: '/src/assets',
+      // helpers: '/src/helpers',
+      // services: '/src/services',
+      // reduxState: '/src/redux',
+    },
+  },
+  base: '/nanny-services/',
+});
